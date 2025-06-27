fb0f84f6022cb365b6bad8607b3ee5b7b5e3413a
Ashu Negi <anegi6723@gmail.com>
2025-06-28 00:01:24 +0530

    created auth middleware and auth routes

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { login, logout, signup } from '../controllers/auth.js';
+import { isAuthenticated } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", isAuthenticated, logout);
+
+
+export default router;
