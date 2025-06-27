b2199217920f628fb8f97b11e0088669a8910805
Ashu Negi <anegi6723@gmail.com>
2025-06-27 22:40:55 +0530

    import bug fix and created user model

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";    
-import connectDB from "./utils/db";
+import connectDB from "./utils/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
---
+++ backend/src/models/user.models.js
@@ -0,0 +1,34 @@
+import { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password : {
+        type: String,
+        required: true,
+    },
+    role : {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    mobile : {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    countryCode : {
+        type: String,
+        required: true,
+    },
+}, {timestamps: true});
+
+const User = mongoose.model('User', UserSchema);
+export default User;
