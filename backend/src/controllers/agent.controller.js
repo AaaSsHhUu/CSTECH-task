47da0a7bb643eae29ca81df3c9d08745d46a06ed
Ashu Negi <anegi6723@gmail.com>
2025-06-28 22:59:06 +0530

    modified agent controller

--- backend/src/controllers/agent.controller.js
+++ backend/src/controllers/agent.controller.js
@@ -1,22 +1,23 @@
+import Agent from "../models/agent.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
+import { sendEmail } from "../utils/nodemailer.js";
 import { randomPasswordGenerator } from "../utils/passwordGenerator.js";
 
 export const createAgent = asyncHandler(async (req, res) => {
     const { name, email, mobile, countryCode, role = "agent" } = req.body;
-
     if ([name, email, mobile, countryCode, role].some(field => field?.trim() === '')) {
         throw new ErrorHandler("Invalid Inputs", 411);
     }
 
-    const existingUser = await User.findOne({ email });
+    const existingUser = await Agent.findOne({ email });
     if (existingUser) {
         throw new ErrorHandler("User already exists with this email", 409);
     }
 
     const password = randomPasswordGenerator();
 
-    const newUser = new User({
+    const newAgent = new Agent({
         name,
         email,
         password,
@@ -25,31 +26,36 @@ export const createAgent = asyncHandler(async (req, res) => {
         role
     });
 
-    if (!newUser) {
+    if (!newAgent) {
         throw new ErrorHandler("User creation failed", 500);
     }
 
-    await newUser.save();
-    
-    // send password to the user via email
-    await sendEmail(email, password);
+    const agentRes = await newAgent.save();
+
+    const emailRes = await sendEmail(newAgent.email, password);
+    console.log("emailRes - ", emailRes)
+
+    if(!emailRes) {
+        await Agent.findByIdAndDelete(agentRes._id); // Clean up if email fails
+        throw new ErrorHandler("Failed to send email", 500);
+    }
 
     res.status(201).json({
         success: true,
         message: "Agent created successfully",
-        user: {
-            id: newUser._id,
-            name: newUser.name,
-            email: newUser.email,
-            mobile: newUser.mobile,
-            countryCode: newUser.countryCode,
-            role: newUser.role
+        agent: {
+            id: newAgent._id,
+            name: newAgent.name,
+            email: newAgent.email,
+            mobile: newAgent.mobile,
+            countryCode: newAgent.countryCode,
+            role: newAgent.role
         }
     });
 });
 
 export const getAllAgents = asyncHandler(async (req, res) => {
-    const agents = await User.find({ role: 'agent' }).select('-password');
+    const agents = await Agent.find({ role: 'agent' }).select('-password');
 
     if (!agents || agents.length === 0) {
         throw new ErrorHandler("No agents found", 404);
@@ -69,7 +75,7 @@ export const getAgentById = asyncHandler(async (req, res) => {
         throw new ErrorHandler("Agent ID is required", 400);
     }
 
-    const agent = await User.findById(id).select('-password');
+    const agent = await Agent.findById(id).select('-password');
 
     if (!agent) {
         throw new ErrorHandler("Agent not found", 404);
@@ -89,7 +95,7 @@ export const deleteAgent = asyncHandler(async (req, res) => {
         throw new ErrorHandler("Agent ID is required", 400);
     }
 
-    const agent = await User.findByIdAndDelete(id);
+    const agent = await Agent.findByIdAndDelete(id);
 
     if (!agent) {
         throw new ErrorHandler("Agent not found", 404);
