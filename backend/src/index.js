4bc74f469760b64a72a958f1c9611f917a31d995
Ashu Negi <anegi6723@gmail.com>
2025-06-28 22:58:37 +0530

    added cookie parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 import cors from "cors";    
 import connectDB from "./utils/db.js";
 import errorMiddleware from "./middlewares/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 // Import routes
 import authRoutes from "./routes/auth.routes.js";
@@ -13,8 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/agent", agentRoutes);
